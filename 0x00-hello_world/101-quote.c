930b2094628ae4a04ed1c8d13d604022768538ae
Amujo Oluyemi <oluyemiamujo@gmail.com>
2022-12-08 11:45:16 -0800

    gone1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int s = strlen("and that piece of art is useful\" ");
 	int s2 = strlen("- Dora Korpar, 2015-10-19\n");
-	s = s+s2;
+	s = s + s2;
 
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
